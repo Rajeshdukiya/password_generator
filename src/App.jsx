dea8fd28b32c2072d95fc3269ac5098408147001
Rajesh Dukiya <112774887+Rajeshdukiya@users.noreply.github.com>
2024-01-11 18:05:01 +0000

    bg_change

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
   },[length, numberAllowed,charAllowed,passwordGenerator])
 
   return (
-    <>
-       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+    <div>
+       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-20 text-orange-500 bg-gray-700'>
         <h1 className='text-white text-center my-3'>Password generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4"> 
           <input
@@ -91,7 +91,7 @@ function App() {
 
         </div>
        </div>
-    </>
+    </div>
   )
 }
 
